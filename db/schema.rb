bb37f6669ac4c16583458c1f1670cbd33cdc6073
ardian idrizi <ardianidizi@gmail.com>
2023-10-16 13:30:04 +0200

    Added Activity,Category and Booking Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_140739) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_16_112650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "activity_name"
+    t.string "address"
+    t.datetime "date_time"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.integer "difficulty"
+    t.string "equipment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_categories_on_activity_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_140739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
+  add_foreign_key "categories", "activities"
 end
---
+++ spec/models/activity_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Activity, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  context 'activity' do
+    it { is_expected.to validate_presence_of :activity_name }
+    it { is_expected.to validate_presence_of :address }
+  end
+end
