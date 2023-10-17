1f1e79ee4ae68634e667e69e93c6f922941c337c
Erwin Adorable <adorable.erwin@gmail.com>
2023-10-17 13:18:21 +0200

    created User,Category,Activity and Booking in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
 puts "Cleaning up database..."
 Booking.destroy_all
 Activity.destroy_all
 Category.destroy_all
 User.destroy_all
-
 puts "Database cleaned"
+
+puts "Building DB..."
+
 puts "Creating users..."
-User.create!(
+
+users = [] # Create an empty array to store the users
+users << User.create!(
   first_name: 'Erwin',
   last_name: 'Doe',
   email: 'erwin@example.com',
   password: '123456'
 )
 
-puts "User created"
+users << User.create!(
+  first_name: 'Ardian',
+  last_name: 'Doe',
+  email: 'ardian@example.com',
+  password: '123456'
+)
+
+users << User.create!(
+  first_name: 'Steven',
+  last_name: 'Doe',
+  email: 'steven@example.com',
+  password: '123456'
+)
+
+users << User.create!(
+  first_name: 'Akane',
+  last_name: 'Doe',
+  email: 'akane@example.com',
+  password: '123456'
+)
+
+puts "Creating categories..."
+categories = ['outdoor', 'indoor'] # Create an array with the categories
+10.times do
+  Category.create!(
+    name: categories.sample # Use sample to select a random category
+  )
+end
+
 puts "Creating activities..."
+difficulty = [1, 2, 3, 4, 5] # Create an array with the difficulties
+10.times do
+  Activity.create!(
+    name: 'Hiking',
+    address: Faker::Address.full_address,
+    date_time: Faker::Time,
+    description: Faker::Lorem.sentence(word_count: 10),
+    owner: users.sample,  # Use sample to select a random user
+    difficulty: difficulty.sample,
+    equipment: Faker::Lorem.word,
+    category: Category.all.sample  # Use Category.first to select the first category
+  )
+end
 
-Category.create!(
-  name: 'outdoor'
-)
+puts "Creating bookings..."
+10.times do
+  Booking.create!(
+    status: true,
+    activity: Activity.all.sample,  # Use Activity.all.sample to select a random activity
+    participant: User.all.sample  # Use User.all.sample to select a random user
+  )
+end
 
-Activity.create!(
-  name: 'Hiking',
-  address: 'Rue de la Loi 155, 1040 Brussels',
-  date_time: '2021-10-20 10:00:00',
-  description: 'Hiking in the forest',
-  owner_id: User.first.id,
-  difficulty: 3,
-  equipment: 'Hiking shoes',
-  category_id: Category.first.id
-)
-
-Booking.create!(
-  status: true,
-  activity_id: Activity.first.id,
-  participant_id: User.first.id
-)
-puts "Activity created"
-puts "Finished!"
+puts "DB created"
