0ff395ed0be8f399e383bed969457acbaa426cd0
Steven Marquez <steven192811@gmail.com>
2023-10-18 13:39:24 +0200

    Added Layout to show page and other improvements

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/pages/home', to: 'pages#home'
   get '/pages/show', to: 'pages#show'
 end
