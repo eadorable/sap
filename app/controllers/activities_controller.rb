1e55d3fda0295b344d7dfcfcd2c84aa2cd3158ca
Akane Saito <ukku19@gmail.com>
2023-10-18 12:46:44 +0200

    adding search function

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,8 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
+    if params[:search].present? && params[:search][:query] != ''
+      @activities = Activity.all.global_search(params[:search][:query])
+    end
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,12 @@
 class Activity < ApplicationRecord
   belongs_to :category
   belongs_to :owner, foreign_key: 'owner_id', class_name: 'User'
+
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :address, :description],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
