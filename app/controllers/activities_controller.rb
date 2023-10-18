777b83e1cefdbcb0fe7e907958fcfd503039590c
Erwin Adorable <adorable.erwin@gmail.com>
2023-10-18 17:08:26 +0200

    added new and create method and new view for Activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,24 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.owner = current_user
+    if @activity.save
+      redirect_to activities_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :address, :date_time, :description, :difficulty, :equipment, :category_id, :owner_id)
+  end
 end
