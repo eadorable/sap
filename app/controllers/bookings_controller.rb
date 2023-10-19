11874d0c5a02a0899e85e5389e909f9e2af5fbcd
Erwin Adorable <adorable.erwin@gmail.com>
2023-10-19 14:57:58 +0200

    add routes for bookings and addd show page for participant

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,14 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
     @activity = Activity.find(params[:activity_id])
 
     @booking = Booking.new(activity: @activity, participant: current_user)
-    # @booking.activity = @activity
-    # @booking.user = current_user
     if @booking.save!
-      redirect_to activity_path(@activity)
+      redirect_to booking_path(@booking)
     else
       render "activities/show"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :activities do
-    resources :bookings, only: %i[new create]
+    resources :bookings, only: %i[create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookings, only: %i[show]
 end
