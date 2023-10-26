04fa545d71fb0e7260efbc4c440441276251169c
Erwin Adorable <adorable.erwin@gmail.com>
2023-10-26 08:53:52 +0200

    auto-genarated ActiveRecord::Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_120326) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_131408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +75,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_120326) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_chatrooms_on_booking_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_120326) do
   add_foreign_key "activities", "users", column: "owner_id"
   add_foreign_key "bookings", "activities"
   add_foreign_key "bookings", "users", column: "participant_id"
+  add_foreign_key "chatrooms", "bookings"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
