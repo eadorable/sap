eecfbb7ffe1e6bb6c42108ef70806cdb6a2ca36f
ardian idrizi <ardianidizi@gmail.com>
2023-10-30 14:32:16 +0100

    Acordions

--- app/javascript/controllers/bloober_controller.js
+++ app/javascript/controllers/bloober_controller.js
@@ -2,12 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="bloober"
 export default class extends Controller {
+  static targets = ["button", "dropdown"]
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+    // console.log("Hello, Stimulus!", this.element);
+    // console.log(this.Demo1Target);
+    console.log(this.Demo1Target);
   }
 
   send(event) {
     event.preventDefault();
     console.log(event.currentTarget);
+  }
+
+  myFunction() {
+    this.dropdownTarget.classList.toggle("d-none");
   }
 }
