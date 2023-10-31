d44c441f41cd755743f1daea1e062053835e6caa
Steven Marquez <steven192811@gmail.com>
2023-10-31 10:26:40 +0100

    Added profile pictures to users and modified the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings, foreign_key: 'participant_id', class_name: 'Booking'
   has_many :activities, foreign_key: 'owner_id', class_name: 'Activity'
+  has_one_attached :profile_image
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
