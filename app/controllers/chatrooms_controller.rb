6ff386b9aca90f7193130bf3565b6b642361adc0
ardian idrizi <ardianidizi@gmail.com>
2023-10-31 12:48:44 +0100

    mobile-navbar function

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,16 @@ class ChatroomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @chatrooms = Chatroom.all
+    # Lets find all the activities where I am the owner
+    @my_activities = Activity.where(owner: current_user).pluck(:id)
+    # Lets find all the booking that I am aproved and the ids of the acitivity
+    @my_booked_activities = current_user.bookings.where(status: true).pluck(:activity_id)
+    @final_ids = []
+    # Empty array
+    @final_ids.concat(@my_activities, @my_booked_activities)
+    # Joining the ids where i am the owner where I am the owner and participant
+    @chatrooms = Chatroom.where(activity_id: @final_ids)
+    # with these of array we retrive all the chatrooms where I am the owner and participant
   end
 
   def show
